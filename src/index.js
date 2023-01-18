cd27c90d44bfc09bdfaeee8e05d98b26e01f162b
Muhammad Aleem Hashmi <aleemhashmi4321@gmail.com>
2023-01-18 19:25:12 +0500

    heading added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-function component() {
+import './style.css';
+
+const component = () => {
   const element = document.createElement('div');
-  element.innerHTML = '<p>Hello World!</p>';
+  element.innerHTML = '<p>To Do List</p>';
+  element.classList.add('todo');
   return element;
-}
+};
 
 document.body.appendChild(component());
