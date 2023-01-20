b8cc63070c86fa9a83c334f45e5e06c675822157
Your Name <you@example.com>
2023-01-20 11:46:20 +0500

    added add and remove function with dynamic data

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
-// import css
+import Task from './modules/crud.js';
 import './style.css';
-// import javaScript from modules
-import toDoList from './modules/list.js';
-// call elements
-const listContainer = document.querySelector('.list-container');
 
-const toDos = () => {
-  toDoList.forEach((item) => {
-    listContainer.innerHTML += `
-    <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="${item.index}">
-        <label class="form-check-label">
-            ${item.description}
-        </label>
+const objTask = new Task();
+const inTask = {};
+
+if (localStorage.savedTasks) {
+  objTask.tasks = JSON.parse(localStorage.getItem('savedTasks'));
+}
+
+const container = document.querySelector('.container');
+const ulElement = document.querySelector('.all-todos');
+const inputElement = document.querySelector('.form-input');
+const btnCom = document.createElement('button');
+btnCom.innerHTML = 'Clear all completed';
+btnCom.classList.add('btnCom');
+
+inputElement.addEventListener('change', () => {
+  inTask.description = inputElement.value;
+  inTask.completed = false;
+  inTask.index = objTask.tasks.length;
+  objTask.addTask(new Task(inTask.description, inTask.completed, inTask.index));
+});
+
+window.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    inputElement.value = '';
+  }
+});
+
+btnCom.addEventListener('click', () => {
+  const result = objTask.tasks.filter((task) => task.completed === false);
+  objTask.tasks = result;
+  objTask.populateField();
+  ulElement.innerHTML = '';
+  container.innerHTML = `
+    <div class="title">
+      <p>Today's To Do</p><i class="fas fa-sync-alt"></i>
     </div>
-    <hr>
-    `;
-  });
-};
+    <div class="todo-section">
+      <ul class='all-todos'></ul>
+    </div>
+  `;
+  container.append(objTask.showTasks(), btnCom);
+});
 
-toDos();
+objTask.showTasks();
+container.append(btnCom);
