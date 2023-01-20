cd3c08fede513834ba9376d2f361846692dec10f
Your Name <you@example.com>
2023-01-20 14:07:00 +0500

    added proper style and drag option

--- src/index.js
+++ src/index.js
@@ -1,48 +1,13 @@
-import Task from './modules/crud.js';
+import TodoList from './modules/todoList.js';
+import Form from './modules/form.js';
 import './style.css';
 
-const objTask = new Task();
-const inTask = {};
+window.onload = () => {
+  const myList = new TodoList('ul');
 
-if (localStorage.savedTasks) {
-  objTask.tasks = JSON.parse(localStorage.getItem('savedTasks'));
-}
-
-const container = document.querySelector('.container');
-const ulElement = document.querySelector('.all-todos');
-const inputElement = document.querySelector('.form-input');
-const btnCom = document.createElement('button');
-btnCom.innerHTML = 'Clear all completed';
-btnCom.classList.add('btnCom');
-
-inputElement.addEventListener('change', () => {
-  inTask.description = inputElement.value;
-  inTask.completed = false;
-  inTask.index = objTask.tasks.length;
-  objTask.addTask(new Task(inTask.description, inTask.completed, inTask.index));
-});
-
-window.addEventListener('keyup', (e) => {
-  if (e.key === 'Enter') {
-    inputElement.value = '';
-  }
-});
-
-btnCom.addEventListener('click', () => {
-  const result = objTask.tasks.filter((task) => task.completed === false);
-  objTask.tasks = result;
-  objTask.populateField();
-  ulElement.innerHTML = '';
-  container.innerHTML = `
-    <div class="title">
-      <p>Today's To Do</p><i class="fas fa-sync-alt"></i>
-    </div>
-    <div class="todo-section">
-      <ul class='all-todos'></ul>
-    </div>
-  `;
-  container.append(objTask.showTasks(), btnCom);
-});
-
-objTask.showTasks();
-container.append(btnCom);
+  const myForm = new Form(myList, {
+    newTaskInput: 'new_task_input',
+    newTaskIcon: 'new_task_icon',
+    clearAllCompletedTasks: 'clear_all_completed_tasks',
+  });
+};
